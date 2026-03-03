70f3fdfc5f402d2e4887f05920eafdedc74b7fa6
JAVIER VAZQUEZ GOMEZ <javvazgom@alu.edu.gva.es>
2026-03-03 12:41:14 +0100

    Función mostrarGastosAgrupadosWeb

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,85 @@
+function mostrarDatoEnId(idElemento, valor) {
+    const elemento = document.getElementById(idElemento);
+    
+    if (elemento) {
+        elemento.textContent = valor;
+    } else {
+        console.warn(`No se encontró ningún elemento con el id: ${idElemento}`);
+    }
+}
+
+function mostrarGastoWeb(idElemento, gasto) {
+    const elemento = document.getElementById(idElemento);
+
+    if (!elemento) {
+        console.warn(`No se encontró ningún elemento con el id: ${idElemento}`);
+        return;
+    }
+
+    // Obtenemos la fecha en formato legible
+    const fechaGasto = new Date(gasto.fecha).toLocaleDateString();
+
+    // Creamos el div y spans de las etiquetas
+    const etiquetasGasto = gasto.etiquetas.map (etiqueta =>
+         `<span class="gasto-etiquetas-etiqueta">${etiqueta}</span>`).join('\n');
+
+    // Creamos el HTML del gasto
+    const htmlGasto = `
+        <div class="gasto">
+            <div class="gasto-descripcion">${gasto.descripcion}</div>
+            <div class="gasto-fecha">${fechaGasto}</div>
+            <div class="gasto-valor">${gasto.importe} €</div>
+            <div class="gasto-etiquetas">${etiquetasGasto}</div>
+        </div>
+    `;
+
+    // Insertamos el HTML del gasto en el elemento
+    elemento.innerHTML += htmlGasto;
+}
+
+function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
+    const elemento = document.getElementById(idElemento);
+    if (!elemento) {
+        console.warn(`No se encontró ningún elemento con el id: ${idElemento}`);
+        return;
+    }
+
+    // Traducir el periodo a un texto legible
+    const periodoTraducido = {
+        'dia': "día",
+        'mes': "mes",
+        'anyo': "año"
+    }
+    const periodoTexto = periodoTraducido[periodo] || periodo;
+
+    // HTML para para entrada del valro agrup.
+    // Usamos object.entries para iterar sobre las claves (periodos) y valores (gastos) del objeto agrup.datos
+    const datosHTML = Object.entries(agrup.datos).map(([periodo, gasto]) => `
+        <div class="agrupacion-dato">
+            <span class="agrupacion-dato-clave">${periodo}</span>
+            <span class="agrupacion-dato-valor">${gasto} €</span>
+        </div>
+    `).join('\n');
+
+    const htmlGrupo = `
+        <div class="agrupacion">
+            <h1>Gastos agrupados por ${periodoTexto}</h1>
+            <div class="agrupacion-datos">
+                ${datosHTML}
+            </div>
+        </div>
+    `;
+
+    // Insertamos el HTML de la agrupación en el elemento
+    elemento.innerHTML = htmlGrupo;
+}
+
+
+// NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
+// Las funciones y objetos deben tener los nombres que se indican en el enunciado
+// Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
+export   {
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+}
